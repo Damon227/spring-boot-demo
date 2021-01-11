c169f6cb28f5193dd0c6388e7349a231f00268af
袁成满 <damon227@outlook.com>
2021-01-11 15:17:58 +0800

    first commit

---
+++ spring-boot-enum/src/main/java/pers/ycm/common/enums/BizStatusEnum.java
@@ -0,0 +1,32 @@
+package pers.ycm.common.enums;
+
+/**
+ * 数据状态枚举
+ *
+ * @author yuanchengman
+ * @date 2021-01-11
+ */
+public enum BizStatusEnum implements CommonEnum {
+    NONE(0, ""),
+    NORMAL(1, "正常"),
+    FROZEN(2, "冻结"),
+    DELETED(3, "删除");
+
+    private int code;
+    private String desc;
+
+    BizStatusEnum(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    @Override
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public String getDesc() {
+        return desc;
+    }
+}
---
+++ spring-boot-enum/src/main/java/pers/ycm/common/enums/CommonEnum.java
@@ -0,0 +1,57 @@
+package pers.ycm.common.enums;
+
+import java.util.EnumSet;
+import java.util.Objects;
+
+/**
+ * 通用枚举接口，提供方法进行枚举转换。
+ *
+ * @author yuanchengman
+ * @date 2021-01-11
+ */
+public interface CommonEnum {
+
+
+    /**
+     * 获取枚举值
+     *
+     * @return
+     */
+    int getCode();
+
+    /**
+     * 获取枚举描述
+     *
+     * @return
+     */
+    String getDesc();
+
+    /**
+     * 通过枚举值获取枚举
+     *
+     * @param code  枚举值
+     * @param clazz
+     * @param <E>
+     * @return
+     */
+    static <E extends Enum<E> & CommonEnum> E getEnum(Integer code, Class<E> clazz) {
+        Objects.requireNonNull(code);
+        EnumSet<E> allEnums = EnumSet.allOf(clazz);
+        E myEnum = allEnums.stream().filter(t -> t.getCode() == code).findFirst().orElseThrow();
+        return myEnum;
+    }
+
+    /**
+     * 通过枚举值获取枚举
+     *
+     * @param code  枚举值
+     * @param clazz
+     * @param <E>
+     * @return
+     */
+    static <E extends Enum<E> & CommonEnum> E getEnum(int code, Class<E> clazz) {
+        EnumSet<E> allEnums = EnumSet.allOf(clazz);
+        E myEnum = allEnums.stream().filter(t -> t.getCode() == code).findFirst().orElseThrow();
+        return myEnum;
+    }
+}
